826f81c82cbb9fee2d1ba03d4e053fffa6841a0c
Max Hansen <max.95hansen@gmail.com>
2022-03-30 13:50:59 +0200

    Structure folders for classes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Configuration;
 
-
 class Program
 {
     static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
